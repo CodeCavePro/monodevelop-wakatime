2bfbfd04220a26866bd684d657252fd046e63877
Zhmayev Yaroslav <salaros@yandex.ru>
2016-01-05 19:52:24 +0300

    bumping addin version to v1.0.2

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("MonoDevelop")]
 [assembly: AssemblyTitle("WakaTime")]
 [assembly: AssemblyDescription("WakaTime is a productivity & time tracking tool for programmers. Once the WakaTime plugin is installed, you get a dashboard with reports about your programming by time, language, project, and branch.")]
-[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyVersion("1.0.2")]
 [assembly: AssemblyCopyright("MIT")]
