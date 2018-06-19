9daefbfb7b2a49b71b60f6cd455d71554f6ff7f9
salaros <salaros@yandex.ru>
2018-06-19 21:33:19 +0300

    bumping 7.x version to v3.0.3

--- src/Properties/AssemblyInfo.cs
+++ src/Properties/AssemblyInfo.cs
@@ -3,5 +3,5 @@ using System.Reflection;
 [assembly: AssemblyProduct("MonoDevelop")]
 [assembly: AssemblyTitle("WakaTime")]
 [assembly: AssemblyDescription("WakaTime is a productivity & time tracking tool for programmers. Once the WakaTime plugin is installed, you get a dashboard with reports about your programming by time, language, project, and branch.")]
-[assembly: AssemblyVersion("3.0.2")]
+[assembly: AssemblyVersion("3.0.3")]
 [assembly: AssemblyCopyright("MIT")]
